efbb2d5ac4f49c0d5cd36fa427d18e1dd35c14a1
jrgolden01 <jillian.golden@jjay.cuny.edu>
2021-02-22 21:06:08 +0000

    Code

--- main.cpp
+++ main.cpp
@@ -5,11 +5,66 @@
 //------------------------------
 
 // Use this enum class for indicating the lat/long direction
-enum class Compass {N, S, W, E};
+enum class Compass{N,S,W,E};
 
 // Write your code here
-class GPS {
+class GPS{
+private: 
+  double latitude;
+  double longitude;
+  Compass LatitudeDirection; 
+  Compass LongitudeDirection; 
 
+public:
+double getLongitude(){
+  return longitude;
+}
+double getLatitude(){
+  return latitude;
+}
+Compass getLongitudeDirection(){
+  return LongitudeDirection;
+}
+Compass getLatitudeDirection(){
+  return LatitudeDirection;
+}
+
+GPS(){
+  latitude = 0;
+  longitude = 0;
+  LatitudeDirection = Compass::N;
+  LongitudeDirection = Compass::W;
+}
+
+GPS(double latitude, double longitude){
+if (latitude >=0 && latitude <=90)
+  this -> latitude = latitude;
+else 
+  this -> latitude = 0;
+if (longitude >=0 && longitude<=180)
+  this -> longitude = longitude;
+else longitude = 0;
+  LatitudeDirection = Compass::N;
+  LongitudeDirection = Compass::W;
+}
+
+GPS(double latitude, double longitude,Compass LatitudeDirection, Compass LongitudeDirection){
+if (latitude >=0 && latitude <=90){
+  this -> latitude = latitude;
+  this ->LatitudeDirection = LatitudeDirection;
+}
+else{
+  this -> latitude = 0;
+  this -> LatitudeDirection = Compass::N;
+}
+if (longitude >=0 && longitude<=180){
+  this -> longitude = longitude;
+  this -> LongitudeDirection = LongitudeDirection;
+}
+else{
+  this -> longitude = 0;
+  this -> LongitudeDirection = Compass::W;
+}
 };
 
 //------------------------------
